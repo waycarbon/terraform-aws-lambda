b69fc34e400509f1acd3ae596169232cf3185d12
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-09-23 22:05:14 -0300

    ignoring image changes

--- main.tf
+++ main.tf
@@ -123,6 +123,10 @@ resource "aws_lambda_function" "this" {
     aws_iam_role_policy_attachment.vpc,
     aws_iam_role_policy_attachment.tracing,
   ]
+
+  lifecycle {
+    ignore_changes = [image_uri]
+  }
 }
 
 resource "aws_lambda_layer_version" "this" {
